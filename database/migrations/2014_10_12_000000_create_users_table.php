0e0b90b416628d8e330699c1df37f501a43732bc
Jiang Boyu <jbyjiangboyu@126.com>
2016-05-03 23:32:19 +0800

    Starting migration, edit master layout

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->timestamps();
+
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('cellphone')->unique();
             $table->string('password', 60);
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
---
+++ database/migrations/2016_05_03_142252_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('title');
+            $table->string('author');
+            $table->string('published');
+            $table->string('cover');
+            $table->mediumText('text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('employees');
+    }
+}
