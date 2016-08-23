0a506bd1d5645edeaba4531d753edbb2c725349c
Jiang Boyu <jbyjiangboyu@126.com>
2016-08-23 14:43:30 +0800

    Updating testing form for gprs testing

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,9 +10,16 @@ class TestController extends Controller
 {
     public function getData(Request $request) {
         $test = new \App\Test();
-        $test->type = $_GET['type'];
-        $test->temp_1 = $_GET['temp_1'];
-        $test->humi_1 = $_GET['humi_1'];
+        $test->type = $_GET['tp'];
+        $test->temperature_1 = $_GET['t1'];
+        $test->humidity_1 = $_GET['h1'];
+
+        $test->critical_error = $_GET['cr'];
+
+        $test->compressor_1 = $_GET['c0'];
+        $test->fan_1 = $_GET['c4'];
+        $test->heater_1 = $_GET['c6'];
+        $test->humidifier = $_GET['c8'];
 
         $test->save();
         return '200';
--- database/migrations/2016_08_13_112401_create_tests_table.php
+++ database/migrations/2016_08_13_112401_create_tests_table.php
@@ -16,8 +16,17 @@ class CreateTestsTable extends Migration
             $table->increments('id');
             $table->timestamps();
             $table->text('type');
-            $table->text('temp_1');
-            $table->text('humi_1');
+            $table->text('temperature_1');
+            $table->text('humidity_1');
+            $table->text('temperature_2');
+            $table->text('humidity_2');
+            $table->text('compressor_1');
+            $table->text('compressor_2');
+            $table->text('exchanger_1');
+            $table->text('exchanger_2');
+            $table->text('heater_1');
+            $table->text('heater_2');
+            $table->text('humidifier');
         });
     }
 
