8ce155971622c164e60b1e1bea787d744d8eb5d9
viztor <viztor@outlook.com>
2017-04-30 10:01:15 +0800

    Create stock create

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -18,4 +18,50 @@ class StockController extends Controller
         $current_stocks = \App\Stock::where('remain_amount', '>', 0)->get();
         return \View::make('stock.overview', compact('user', 'employee', 'privilege', 'current_stocks'));
     }
+
+
+
+    // create router
+    public function getCreate(Request $request) {
+        list($user, $employee, $privilege) = \App\Privilege::privilegeAuth();
+        if(sizeof($privilege)==0 || ($privilege->master_admin==0 && $privilege->produce==0))
+        {
+            \Session::flash('danger', '您没有权限访问此页面！(Error: 403 Forbidden)');
+            return redirect('/');
+        }
+
+        return view('stock.create', compact('user', 'employee', 'privilege'));
+    }
+
+    // create new stock unit
+    public function postCreate(Request $request) {
+        list($user, $employee, $privilege) = \App\Privilege::privilegeAuth();
+        if(sizeof($privilege)==0 || ($privilege->master_admin==0 && $privilege->produce==0))
+        {
+            \Session::flash('danger', '您没有权限访问此页面！(Error: 403 Forbidden)');
+            return redirect('/');
+        }
+
+        #data validation
+        $this->validate($request, [
+            'name' => 'required',
+            'category' => 'required',
+            'brand' => 'required',
+            'serial_number' => 'required',
+            'purchase_day' => 'date_format:"Y/m/d"',
+            'purchase_amount' => 'required|integer',
+        ]);
+
+        # data stripping
+        $data = $request->only('name', 'category', 'brand', 'serial_number', 'purchase_day', 'purchase_amount');
+
+        # additional data field edit privilege for admin
+
+
+        $stock = \App\Stock::create($data);
+        $stock->remain_amount = $stock->purchase_amount;
+        $stock->save();
+        \Session::flash('success', '成功创建了新的库存单元。');
+        return redirect('/stock');
+    }
 }
--- app/Stock.php
+++ app/Stock.php
@@ -9,4 +9,8 @@ class Stock extends Model
     public function produces() {
         return $this->belongsToMany('\App\Produce')->withPivot('use_amount')->withTimestamps();
     }
+
+    protected $fillable = [
+        'name', 'category', 'brand', 'serial_number', 'purchase_day', 'purchase_amount'
+    ];
 }
