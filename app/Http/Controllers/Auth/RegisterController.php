00ce245c856ba4b6675078c8b335058e10f09927
Boyu Jiang <jbyjiangboyu@126.com>
2017-05-01 22:21:12 +0800

    Migrate: to laravel 5.3

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -45,12 +45,16 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
+
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
+            'username' => 'required|max:30|unique:users',
+            'last_name' => 'required|max:30',
+            'first_name' => 'required|max:30',
+            'email' => 'required|email|max:100|unique:users',
+            'cellphone' => 'required|numeric|digits_between:2,30|unique:users',
+            'password' => 'required|confirmed|min:6',
         ]);
     }
 
@@ -60,12 +64,15 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return User
      */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
+     protected function create(array $data)
+     {
+         return User::create([
+             'username' => $data['username'],
+             'last_name' => $data['last_name'],
+             'first_name' => $data['first_name'],
+             'email' => $data['email'],
+             'cellphone' => $data['cellphone'],
+             'password' => bcrypt($data['password']),
+         ]);
+     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+/*
 Route::get('/login', 'Auth\AuthController@getLogin');
 Route::post('/login', 'Auth\AuthController@postLogin');
 Route::get('/register', 'Auth\AuthController@getRegister');
 Route::post('/register', 'Auth\AuthController@postRegister');
 Route::get('/logout', 'Auth\AuthController@logout');
+*/
+Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
 
