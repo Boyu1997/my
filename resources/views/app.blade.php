fa2571c63dcc615d2dfe47af83a827495cbf62e6
viztor <viztor@outlook.com>
2017-05-02 14:15:49 +0800

    Update: Separate master layout and framework files.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>
+            @yield('title','艾美康办公系统')
+        </title>
+
+
+        <script src="/js/app.js" charset="utf-8"></script>
+        <link href="/css/app.css" type='text/css' rel='stylesheet'>
+        {{-- <link rel="stylesheet" href="/css/layouts/master.css"> --}}
+        @yield('style')
+        @yield('head')
+
+    </head>
+
+    <body>
+
+        @if(Session::get('success')!=null)
+            <div class="flash_message alert alert-success" role="alert">
+                {{Session::get('success')}}
+            </div>
+        @endif
+        @if(Session::get('danger')!=null)
+            <div class="flash_message alert alert-danger" role="alert">
+                {{Session::get('danger')}}
+            </div>
+        @endif
+
+        @yield('main')
+
+    </body>
+    @yield('js')
+
+</html>
